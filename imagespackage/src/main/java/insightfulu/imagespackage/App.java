e4036ab8380a645ff603d5bb16f81abc006d0769
TheGreatGatley <danielgatty090@gmail.com>
2024-03-08 16:10:08 +0000

    Initial display of an image

--- imagespackage/src/main/java/insightfulu/imagespackage/App.java
+++ imagespackage/src/main/java/insightfulu/imagespackage/App.java
@@ -2,10 +2,15 @@ package insightfulu.imagespackage;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -17,8 +22,19 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        ImageView imageView = new ImageView();
+        File file = new File("src/main/resources/images/stockimage.jpg");
+        Image image = new Image(new FileInputStream(file));
+        imageView.setImage(image);
+
+        Group root = new Group(imageView);
+
+        scene = new Scene(root, 800, 600);
+
+        stage.setTitle("Loading an image");
+
         stage.setScene(scene);
+
         stage.show();
     }
 
---
+++ imagespackage/src/main/java/insightfulu/imagespackage/SingleImage.java
@@ -0,0 +1,7 @@
+package insightfulu.imagespackage;
+
+import javafx.scene.image.ImageView;
+
+public class SingleImage extends ImageView {
+
+}
